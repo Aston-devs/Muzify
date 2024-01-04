b357a4a0fd972974f323d53258d3b7c1fcc4d4bf
Vladimir Glinskikh <englearn48@gmail.com>
2024-01-04 23:43:38 +0300

    Feature: Flyway and User Entity (#9)
    Feature: Flyway and User Entity

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,8 +1,13 @@
+val lombokVersion = "1.18.30"
+
 dependencies {
+    annotationProcessor("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.flywaydb:flyway-core")
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
 }
---
+++ user-service/src/main/java/ru/musify/userservice/model/User.java
@@ -0,0 +1,31 @@
+package ru.musify.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+}
