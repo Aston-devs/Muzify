1dd4e60536378e36ed3af877337b5f11f100b2a9
Leonid Romanov <romanovleonid82@gmail.com>
2024-01-10 01:54:43 +0300

    Feature: Establish Front Service

---
+++ musify-front-service/src/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./musify-logo.svg";
+import musify from "./Musify.svg";
+import "./Header.css";
+import LoginBtn from "../loginBtn/LoginBtn";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <img className="musify-logo" src={logo} alt="Musify logo" />
+        <LoginBtn />
+      </div>
+      <div className="header-musify-word">
+        <img src={musify} alt="header musify word" />
+      </div>
+      <div className="header-tagline">
+        Don't miss a day without your favourite music
+      </div>
+    </div>
+  );
+}
