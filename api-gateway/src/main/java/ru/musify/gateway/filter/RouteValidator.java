a9a030886e995066c5d01e3a89dc0f75601445f0
Данил Токарев <danil2002tokarev@mail.ru>
2024-01-16 17:15:09 +0300

    Spring Cloud api-gateway with JWT Security

--- api-gateway/src/main/java/ru/musify/gateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/ru/musify/gateway/filter/RouteValidator.java
@@ -2,10 +2,12 @@ package ru.musify.gateway.filter;
 
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.function.Predicate;
 
+@Component
 public class RouteValidator {
     public static final List<String> OPEN_API_ENDPOINTS = List.of(
             "/api/v1/musify/auth/signup",
