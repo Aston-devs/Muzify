f5c110d95ce33a1558ef9505e4f0d22c9aa80e1f
Данил Токарев <danil2002tokarev@mail.ru>
2024-01-16 17:17:36 +0300

    Spring Cloud api-gateway with JWT Security

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
 
     implementation ("org.springframework.boot:spring-boot-starter-webflux")
     implementation ("org.springframework.boot:spring-boot-starter-actuator")
-    implementation ("org.springframework.boot:spring-boot-starter-security")
     implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation ("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation ("io.jsonwebtoken:jjwt-jackson:0.11.5")
