b2a0a9465ff701e187508a958de1b4b5d91a888b
Данил Токарев <danil2002tokarev@mail.ru>
2024-01-18 18:27:52 +0300

    Api-gateway was fix

--- api-gateway/src/main/java/ru/musify/gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/ru/musify/gateway/filter/AuthenticationFilter.java
@@ -3,6 +3,7 @@ package ru.musify.gateway.filter;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.musify.gateway.util.JWTUtil;
 
@@ -38,6 +40,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     public GatewayFilter apply(Config config) {
         return (((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
             if (validator.isSecured.test(request)) {
                 if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                     throw new RuntimeException("missing authorization header");
@@ -49,6 +52,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                     try {
                         String userID = jwtUtil.validateToken(authHeader);
                         request.mutate().header("loggedInUser", userID).build();
+                        response.writeWith(Flux.just(new DefaultDataBufferFactory().wrap(userID.getBytes())));
                     } catch (JWTVerificationException e) {
                         throw new RuntimeException("un authorized access to application");
                     }
@@ -56,7 +60,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
 
 
             }
-            return chain.filter(exchange.mutate().request(request).build());
+            return chain.filter(exchange.mutate().request(request).response(response).build());
         }));
     }
 
